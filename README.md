345aec018230d763dfd926cd91e6c3bdf92ee0cc
Titus <jordane@hive.fi>
2019-08-12 19:38:57 +0300

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# slack-bulkinviter
+
+Super quick and dirty Golang script to bulk invite ALL users in a slack team to a specific channel. We need to do this regulary with [HiveHelsinki](http://hive.fi) when we create a new channel that everyone should be in.
+
+To use:
+* export your SLACK_TOKEN
+* compile it : go build -o inviter main.go
+* ./inviter -c <channel_id>
+* sit back and let it do its work
+
+Inspired a lot on: https://github.com/robby-dermody/slack-bulkinviter but this version take care to check if it's a public channel (channel), a private channel (group) or a public channel which has been turned to a private one (conversation) in the time.
---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/nlopes/slack"
+	"os"
+)
+
+func GetTypeChan(api *slack.Client, id string) string {
+	var err error
+
+	_, err = api.GetChannelInfo(id)
+	if err == nil {
+		return "channel"
+	}
+
+	_, err = api.GetGroupInfo(id)
+	if err == nil {
+		return "group"
+	}
+
+	_, err = api.GetConversationInfo(id, false)
+	if err == nil {
+		return "conversation"
+	}
+	return ""
+
+}
+
+func main() {
+	chanID := flag.String("c", "", "channel/groups ID where you want to invite everyone")
+
+	slackToken := os.Getenv("SLACK_TOKEN")
+	if len(slackToken) == 0 {
+		fmt.Println("SLACK_TOKEN environment variable not set")
+		return
+	}
+
+	flag.Parse()
+	if len(*chanID) == 0 {
+		flag.PrintDefaults()
+		return
+	}
+
+	api := slack.New(slackToken)
+
+	users, err := api.GetUsers()
+	if err != nil {
+		return
+	}
+
+	switch GetTypeChan(api, *chanID) {
+	case "channel":
+		for _, user := range users {
+			api.InviteUserToChannel(*chanID, user.ID)
+			fmt.Printf("Inviting %s into the channel\n", user.Name)
+		}
+	case "group":
+		for _, user := range users {
+			api.InviteUserToGroup(*chanID, user.ID)
+			fmt.Printf("Inviting %s into the group\n", user.Name)
+		}
+	case "conversation":
+		for _, user := range users {
+			api.InviteUsersToConversation(*chanID, user.ID)
+			fmt.Printf("Inviting %s into the conversation\n", user.Name)
+		}
+	}
+}
